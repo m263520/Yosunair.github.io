7827eb43c941d006d2622c75dc642ea0b51b9f1e
迷途之女 希洛尔 <3297161149@qq.com>
2023-07-12 11:21:49 +0800

    修复

--- doc/.vuepress/config.ts
+++ doc/.vuepress/config.ts
@@ -6,14 +6,14 @@ export default defineUserConfig({
 
   locales: {
     "/": {
-      lang: "zh-CN",
-      title: "尤苏奈尔基本外发",
-      description: "欢迎来到尤苏奈尔基本外发，来尽情探索吧！",
-    },
-    "/zh-TW/": {
       lang: "zh-TW",
       title: "尤蘇奈爾基本外發",
       description: "歡迎來到尤蘇奈爾基本外發，來盡情探索吧！",
+    },
+    "/zh-CN/": {
+      lang: "zh-CN",
+      title: "尤苏奈尔基本外发",
+      description: "欢迎来到尤苏奈尔基本外发，来尽情探索吧！",
     },
   },
 
